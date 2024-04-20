1c5873fd20b1ff7522bab6ef07aaf05d132e629b
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-04-20 19:13:54 +0000

    updated the navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+  return (
+    <div id="whole-wheat" className="px-5">
+      <nav className="navbar navbar-expand-lg d-flex justify-content-between">
+        <div className="d-flex justify-content-between w-100">
+          <Link
+            className="navbar-brand"
+            to={store.user ? "/profile" : "/"}
+            style={{ fontSize: '30px', color: 'forestGreen' }}
+          >
+            NourishNav
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse flex-grow-0"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/macro">
+                  Nutrition
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Recipe">
+                  Recipes
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about_us">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link profile-link" to="/profile">
+                  Profile
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
